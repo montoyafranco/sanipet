1aa133f1458a9629150511acf3769f9666d1bc93
montoyafranco <francomontoya9810@gmail.com>
2022-07-07 09:09:41 -0300

    Sould start to make the logic

--- src/main/java/Sanipet/Principal.java
+++ src/main/java/Sanipet/Principal.java
@@ -4,5 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Welcome to Sanipet software");
         System.out.println("Follow the instructions to create appointments - get stock  - clinical history");
+
+        // Create logic and option menu here
     }
 }
